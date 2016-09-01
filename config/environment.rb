142ef42dbfae354ebe2f7bf09811a871312e29b4
TheLucasMoore <lucasedwardmoore@gmail.com>
2016-09-01 08:52:24 -0600

    update databases

--- config/environment.rb
+++ config/environment.rb
@@ -6,15 +6,15 @@ Bundler.require(:default, ENV['RACK_ENV'])
 require './app'
 require_all 'models'
 
-# configure :test do
-#   ActiveRecord::Base.establish_connection(
-#     :adapter => "sqlite3",
-#     :database => "db/#{ENV['RACK_ENV']}.sqlite"
-#   )
-# end
+configure :development, :test do
+  ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => "db/#{ENV['RACK_ENV']}.sqlite"
+  )
+end
 
-configure :production, :development, :test do
- db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///anagrammatist_development')
+configure :production do
+ db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///database')
 
  ActiveRecord::Base.establish_connection(
    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
