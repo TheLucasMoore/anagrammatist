9a1534b0813e2bbdfe588f4a2af50b66612b3cb9
TheLucasMoore <lucasedwardmoore@gmail.com>
2016-09-01 08:50:29 -0600

    separate databases

--- config/environment.rb
+++ config/environment.rb
@@ -6,14 +6,14 @@ Bundler.require(:default, ENV['RACK_ENV'])
 require './app'
 require_all 'models'
 
-# configure :test do
-#   ActiveRecord::Base.establish_connection(
-#     :adapter => "sqlite3",
-#     :database => "db/#{ENV['RACK_ENV']}.sqlite"
-#   )
-# end
+configure :development, :test do
+  ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => "db/#{ENV['RACK_ENV']}.sqlite"
+  )
+end
 
-configure :production, :development, :test do
+configure :production do
  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///database')
 
  ActiveRecord::Base.establish_connection(
