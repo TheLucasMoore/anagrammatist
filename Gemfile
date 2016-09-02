c5479635546b27f48f74a772fe8965a7973fbfd6
TheLucasMoore <lucasedwardmoore@gmail.com>
2016-09-01 19:20:21 -0600

    tiny tweaks

--- Gemfile
+++ Gemfile
@@ -13,16 +13,7 @@ gem 'shotgun'
 gem 'activerecord', :require => 'active_record'
 gem 'sinatra-activerecord', :require => 'sinatra/activerecord'
 gem 'rake'
-
-group :test, :development do
-  #sqlite makes local development more simple
-  gem 'sqlite3'
-  gem 'tux'
-  gem 'test-unit'
-  gem 'pry'
-end
-
-group :production do
-  #postgres for Heroku deployment
-  gem 'pg'
-end
+gem 'pg'
+gem 'tux'
+gem 'test-unit'
+gem 'pry'
--- config/environment.rb
+++ config/environment.rb
@@ -6,20 +6,13 @@ Bundler.require(:default, ENV['RACK_ENV'])
 require './app'
 require_all 'models'
 
-configure :development do
- set :database, 'sqlite:///dev.db'
- set :show_exceptions, true
-end
+db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
 
-configure :production do
- db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
-
- ActiveRecord::Base.establish_connection(
-   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-   :host     => db.host,
-   :username => db.user,
-   :password => db.password,
-   :database => db.path[1..-1],
-   :encoding => 'utf8'
- )
-end
+ActiveRecord::Base.establish_connection(
+ :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+ :host     => db.host,
+ :username => db.user,
+ :password => db.password,
+ :database => db.path[1..-1],
+ :encoding => 'utf8'
+)
