76f555cfd7a94ed33dd7ef452d3c92bb7109d298
TheLucasMoore <lucasedwardmoore@gmail.com>
2016-09-01 20:42:11 -0600

    instant search

--- public/js/ng-app/controllers/AppController.js
+++ public/js/ng-app/controllers/AppController.js
@@ -1,6 +1,7 @@
 function AppController($scope, ApiService) {
   $scope.title = "Anagrammatist"
   $scope.search = function() {
+    $scope.results = '';
     ApiService.getAnagrams($scope.anagramSearch)
       .then(res => {
       console.log(res)
